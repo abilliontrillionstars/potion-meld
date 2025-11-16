c1608ae83bcc30871f17247f8beede4cf4e0f324
Laniakea <billiontrillionstars@gmail.com>
2025-11-16 13:52:38 -0500

    add potion colors dict

---
+++ potion-meld/Scripts/recipes.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var colors = {
+	"slow" : Color(23,31,94,255),
+	"intelligence" : Color(74,152,277,255),
+	"fortitude" : Color(134,109,45,255), 
+	"darkvision": Color(63,192,91,255),
+	"slow fall": Color(166,233,198,255),
+	"poison" : Color(107,149,75,255),
+	"sleep" : Color(172,114,237,255),
+	"haste" : Color(219,181,20,255),
+	"strength" : Color(186,64,105,255),
+	"stone skin" : Color(100,100,100,255),
+	"true sight" : Color(235,66,73,255),
+	"feign death" : Color(75,76,42,255),
+	"wings" : Color(189,240,233,255),
+	"healing" : Color(128,17,24,255),
+	"growth": Color(224,193,24,255),
+	"agility": Color(205,205,205,255),
+	"stealth": Color(52,52,52,255),
+	"focus": Color(202,50,101,255),
+	"gills": Color(126,183,228,255),
+	"dangersense": Color(241,231,144,255),
+	"thorns": Color(36,69,50,255)
+}
