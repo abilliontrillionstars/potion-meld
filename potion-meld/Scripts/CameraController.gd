561e615e211c902c6336b30ff097a2eea85104e2
CALeslie <lesliepride3+git@gmail.com>
2025-11-16 16:47:59 -0500

    Changed export presets, modified screen sides to work with look left and right

--- potion-meld/Scripts/CameraController.gd
+++ potion-meld/Scripts/CameraController.gd
@@ -31,59 +31,13 @@ func _start() -> void:
 	stirrer_dragbox.mouse_filter = Control.MouseFilter.MOUSE_FILTER_IGNORE
 
 func _process(delta: float) -> void:
-	if !moving:
-		if (Input.is_action_just_pressed("look_left")):
-			# sound time!
-			$"../SoundManager".find_child("Footsteps").play()
-			curr_transform_x = position.x
-			curr_transform_z = position.z
-			curr_rot = rotation.y
-			if state == 0:
-				pass
-			elif state == 1:
-				target_transform = left_transform
-				target_rot = deg_to_rad(left_y_rot)
-				shelf_hitbox.show_hitboxes()
-				customer_view.hide_order_hitboxes()
-				inventory_popout.open_drawer()
-				inventory_popout.is_open = true
-				moving = true
-				state = 0
-			elif state == 2:
-				target_transform = middle_transform
-				target_rot = deg_to_rad(middle_y_rot)
-				stirrer_dragbox.visible = false
-				stirrer_dragbox.mouse_filter = Control.MouseFilter.MOUSE_FILTER_IGNORE
-				moving = true
-				state = 1
-			customer_view.hide_order_bubbles()
+	if (Input.is_action_just_pressed("look_left")):
+		look_left()
+			
+	if (Input.is_action_just_pressed("look_right")):
+		look_right()
 				
-		if (Input.is_action_just_pressed("look_right")):
-			# sound time!
-			$"../SoundManager".find_child("Footsteps").play()
-			curr_transform_x = position.x
-			curr_transform_z = position.z
-			curr_rot = rotation.y
-			if state == 0:
-				target_transform = middle_transform
-				target_rot = deg_to_rad(middle_y_rot)
-				shelf_hitbox.hide_hitboxes()
-				customer_view.show_order_hitboxes()
-				moving = true
-				state = 1
-			elif state == 1:
-				target_transform = right_transform
-				target_rot = deg_to_rad(right_y_rot)
-				stirrer_dragbox.visible = true
-				stirrer_dragbox.mouse_filter = Control.MouseFilter.MOUSE_FILTER_STOP
-				moving = true
-				state = 2
-			elif state == 2:
-				pass
-			customer_view.hide_order_bubbles()
-			customer_view.visible = false
-				
-	else:
+	if moving:
 		lerp_val += delta
 		rotation.y = lerp_angle(curr_rot, target_rot, lerp_val)
 		position.x = lerp(curr_transform_x, target_transform.x, lerp_val)
@@ -93,3 +47,63 @@ func _process(delta: float) -> void:
 			if state == 1:
 				customer_view.visible = true
 			lerp_val = 0
+
+func look_left():
+	# sound time!
+	$"../SoundManager".find_child("Footsteps").play()
+	curr_transform_x = position.x
+	curr_transform_z = position.z
+	curr_rot = rotation.y
+	if state == 0:
+		pass
+	elif state == 1:
+		target_transform = left_transform
+		target_rot = deg_to_rad(left_y_rot)
+		shelf_hitbox.show_hitboxes()
+		customer_view.hide_order_hitboxes()
+		inventory_popout.open_drawer()
+		inventory_popout.is_open = true
+		moving = true
+		state = 0
+	elif state == 2:
+		target_transform = middle_transform
+		target_rot = deg_to_rad(middle_y_rot)
+		stirrer_dragbox.visible = false
+		stirrer_dragbox.mouse_filter = Control.MouseFilter.MOUSE_FILTER_IGNORE
+		moving = true
+		state = 1
+	customer_view.hide_order_bubbles()
+
+func look_right():
+	# sound time!
+	$"../SoundManager".find_child("Footsteps").play()
+	curr_transform_x = position.x
+	curr_transform_z = position.z
+	curr_rot = rotation.y
+	if state == 0:
+		target_transform = middle_transform
+		target_rot = deg_to_rad(middle_y_rot)
+		shelf_hitbox.hide_hitboxes()
+		customer_view.show_order_hitboxes()
+		moving = true
+		state = 1
+	elif state == 1:
+		target_transform = right_transform
+		target_rot = deg_to_rad(right_y_rot)
+		stirrer_dragbox.visible = true
+		stirrer_dragbox.mouse_filter = Control.MouseFilter.MOUSE_FILTER_STOP
+		moving = true
+		state = 2
+	elif state == 2:
+		pass
+	customer_view.hide_order_bubbles()
+	customer_view.visible = false
+
+
+func _on_look_left_button_pressed() -> void:
+	if !moving:
+		look_left()
+
+func _on_look_right_button_pressed() -> void:
+	if !moving:
+		look_right()
