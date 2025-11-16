408e1ade7c316e01aa5a8b8b573c61b0e195d267
Laniakea <billiontrillionstars@gmail.com>
2025-11-16 16:02:58 -0500

    add drawer sounds

--- potion-meld/Scripts/drawer.gd
+++ potion-meld/Scripts/drawer.gd
@@ -5,14 +5,18 @@ var selected_slot: int = -1
 
 func _on_pressed() -> void:
 	if is_open:
+		$"../../../SoundManager/DrawerOpen".play()
 		self.get_child(0).play_backwards("open")
 	else:
+		$"../../../SoundManager/DrawerClose".play()
 		self.get_child(0).play("open")
 	self.is_open = not self.is_open
 	
 func open_drawer():
 	if !is_open:
+		$"../../../SoundManager/DrawerOpen".play()
 		self.get_child(0).play("open")
+		self.is_open = true
 
 func _on_ready() -> void:
 	self.set_position(Vector2(768, 539))
