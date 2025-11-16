d5a6d14f1a8eeb7cddbcf31e75a0a3915f5572c1
CALeslie <lesliepride3+git@gmail.com>
2025-11-15 22:13:46 -0500

    Added more stub classes with partial functionality, cauldron now has function to modulate colors, various tweaks

---
+++ potion-meld/Scripts/ingredient.gd
@@ -0,0 +1,22 @@
+extends Node
+
+class_name Ingredient
+
+enum ingredient_type { DungeonDust, EyeOfNewt, GrapesOfWrath, PhoenixFeather, GelatinousCubes }
+var type: ingredient_type
+var color: Color
+
+func _ready() -> void:
+    var key = ingredient_type.keys()[randi() % ingredient_type.size()]
+    type = ingredient_type[key]
+
+    if type == 0:
+        color = Color(223, 149, 96, 255)
+    elif type == 1:
+        color = Color(66, 100, 210, 255)
+    elif type == 2:
+        color = Color(169, 66, 210, 255)
+    elif type == 3:
+        color = Color(210, 87, 66, 255)
+    elif type == 4:
+        color = Color(97, 205, 90, 255)
