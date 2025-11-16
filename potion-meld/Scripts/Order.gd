892e2d1d94e8af97013e595e3efe4127bcc1534c
Laniakea <billiontrillionstars@gmail.com>
2025-11-16 15:38:38 -0500

    refactor labelling functions

--- potion-meld/Scripts/Order.gd
+++ potion-meld/Scripts/Order.gd
@@ -1,13 +1,8 @@
 extends Node
 
 class_name NPCOrder
-<<<<<<< HEAD
 
-var order_potion_type = RecipeBook.recipes
-=======
-	
-enum order_potion_type { DarkVision, Thorns, FeignDeath,  Slow, Dangersense, Sleep, Truesight, Intelligence, Haste, Strength, Gills, Stealth, Healing, Focus, Wings, Growth, Stoneskin, Agility}
->>>>>>> a3741d86b23157ed78c4465d2f6d1a7fce018211
+var order_potion_type = RecipeBook.recipes	
 enum order_viscosity_type { VeryThin, Thin, Normal, Thick, VeryThick}
 var order_type
 var order_length: float 
--- potion-meld/Scripts/game_manager.gd
+++ potion-meld/Scripts/game_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var player_score = 0
 @onready var player_score_label = $"../World/HUD/PlayerScore/ScoreLabel"
+@onready var customer_view = $"../World/HUD/CustomerView"
 
 var recipes: Dictionary[String, Array] = RecipeBook.recipes
 
@@ -13,19 +14,23 @@ func add_player_score(amt: int):
 	player_score += amt
 	player_score_label.text = "[wave amp=20.0 freq=2.0 connected=1]Score: " + str(player_score) + "[/wave]"
 
-func get_potion_type(ingr1, ingr2):
+func get_ingr_name(ingr: int) -> String:
+	return Ingredient.ingredient_type.keys()[ingr]
+
+func get_potion_name(ingr1, ingr2):
 	for potion in recipes.keys():
 		var recipe = recipes[potion]
 		if ingr1 in recipe and ingr2 in recipe:
 			return potion
 			
 func attempt_deliver_potion(ingr1, ingr2):
-	var potion_type = get_potion_type(ingr1, ingr2)
+	var potion_type = get_potion_name(ingr1, ingr2)
 	print (potion_type)
 	for i in range(0, NpcManager.counter_npcs.size()):
 		if NpcManager.counter_npcs[i]:
 			if NpcManager.counter_npcs[i].npc_order.order_type == potion_type:
 				GameManager.add_player_score(clamp((40 * (NpcManager.counter_npcs[i].lifetime/ NpcManager.counter_npcs[i].npc_wait_timer)), 0, 9999) + 10)
+				customer_view.hide_order_bubble(i)
 				NpcManager.counter_npcs[i].leave_counter()
 				print ("found a match, award player points")
 			#print (NpcManager.npcs[i].npc_order.order_type)
