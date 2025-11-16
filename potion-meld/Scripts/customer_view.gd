222c941b90bd1ecc83a38f53890878b98cae1631
CALeslie <lesliepride3+git@gmail.com>
2025-11-15 21:10:20 -0500

    Added customer_view to hud scene for proper UI layering

--- potion-meld/Scripts/customer_view.gd
+++ potion-meld/Scripts/customer_view.gd
@@ -10,32 +10,32 @@ extends Control
 func _on_left_customer_mouse_entered() -> void:
     if !NpcManager.counter_spots_avail[0]:
         left_order_bubble.visible = true
-        print ("show left customer order if counter spot taken")
+        #print ("show left customer order if counter spot taken")
 
 func _on_middle_customer_mouse_entered() -> void:
     if !NpcManager.counter_spots_avail[1]:
         middle_order_bubble.visible = true
-        print ("show middle customer order if counter spot taken")
+        #print ("show middle customer order if counter spot taken")
 
 func _on_right_customer_mouse_entered() -> void:
     if !NpcManager.counter_spots_avail[2]:
         right_order_bubble.visible = true
-        print ("show right customer order if counter spot taken")
+        #print ("show right customer order if counter spot taken")
 
 func _on_left_customer_mouse_exited() -> void:
     if !NpcManager.counter_spots_avail[0]:
         left_order_bubble.visible = false
-        print ("hide left customer order if counter spot taken")
+        #print ("hide left customer order if counter spot taken")
 
 func _on_middle_customer_mouse_exited() -> void:
     if !NpcManager.counter_spots_avail[1]:
         middle_order_bubble.visible = false
-        print ("hide middle customer order if counter spot taken")
+        #print ("hide middle customer order if counter spot taken")
 
 func _on_right_customer_mouse_exited() -> void:
     if !NpcManager.counter_spots_avail[2]:
         right_order_bubble.visible = false
-        print ("hide right customer order if counter spot taken")
+        #print ("hide right customer order if counter spot taken")
 
 func update_order_bubble(bubble_id: int, order_info: NPCOrder):
     type_texts[bubble_id].text = "Potion of " + order_info.order_type
